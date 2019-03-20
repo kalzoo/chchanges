48abb6d712362373b4d482ad9de0a668bfeefb1b
Jonathan Ward <jonathan@rigetti.com>
2019-03-20 10:24:57 -0700

    fine-tuning

--- changes/test/test_bayesian_online.py
+++ changes/test/test_bayesian_online.py
@@ -4,14 +4,14 @@ from changes.bayesian_online import ConstantHazard, StudentT, Detector
 
 def test_detector():
     normal_signal = np.random.normal(loc=50e-6, scale=1e-6, size=1000)
-    normal_signal[250:500] += 150e-6
-    normal_signal[500:750] -= 150e-6
-    lambda_ = 250
-    delay = 200
+    normal_signal[250:500] += 30e-6
+    normal_signal[500:750] -= 30e-6
+    lambda_ = 100
+    delay = 150
 
     hazard = ConstantHazard(lambda_)
-    posterior = StudentT(alpha=1., beta=1e-6, kappa=1., mu=50e-6)
-    detector = Detector(hazard, posterior, delay, threshold=0.2)
+    posterior = StudentT(alpha=1., beta=1e-12, kappa=1., mu=50e-6)
+    detector = Detector(hazard, posterior, delay, threshold=0.5)
 
     changepoints = []
     for datum in normal_signal:
